668c5ec88134b13952d9023b59eb3456e64b2558
Mayki Gonçalves <mayki.goncalves@arquivei.com.br>
2024-04-24 15:50:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.primeiroapp
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeiroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BotaoOla.setOnClickListener {
+            val nome : String = binding.editarNome.text.toString()
+            binding.retornoTexto.text = "Olá ${nome}"
+        }
+    }
+}
+
